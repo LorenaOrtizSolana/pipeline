b97c90751b00ff9c50cb8abdd0fad5d3421ba4f1
Lorena Ortiz <a01750283@tec.mx>
2026-05-20 14:04:06 +0200

    Empty values

--- sqlconnection.py
+++ sqlconnection.py
@@ -3,8 +3,12 @@ import csv
 import itertools
 
 conn = sqlite3.connect('test_db.db')
+conn.execute("PRAGMA foreign_keys = ON")
 cursor = conn.cursor()
 
+#### Accounts table
+####
+####
 cursor.execute('''
                CREATE TABLE IF NOT EXISTS accounts
                (
@@ -30,7 +34,14 @@ cursor.execute('''
                )
                    )
                ''')
+####
+####
+####
 
+
+#### Fill accounts table
+####
+####
 cursor.execute('DELETE FROM accounts')
 cursor.execute('DELETE FROM sqlite_sequence WHERE name="accounts"')
 with open('accounts.csv', newline='', encoding='utf-8') as csvfile:
@@ -41,7 +52,23 @@ with open('accounts.csv', newline='', encoding='utf-8') as csvfile:
                        INSERT INTO accounts (Status, Currency, OpeningDate, AccountTypeCode, Timezone)
                        VALUES (?, ?, ?, ?, ?)
                        ''', row[1:])
+    ####
+####
+####
 
+
+#### Untouched Accounts table
+####
+####
+cursor.execute('CREATE TABLE IF NOT EXISTS orig_accounts AS SELECT * FROM accounts')
+####
+####
+####
+
+
+####  Transactions table
+####
+####
 cursor.execute('''
                CREATE TABLE IF NOT EXISTS transactions
                (
@@ -72,9 +99,14 @@ cursor.execute('''
                (
                    AccountId
                )
+                   ON DELETE CASCADE
+                   ON UPDATE CASCADE
                    )
                ''')
 
+#### Fill transactions table
+####
+####
 cursor.execute('DELETE FROM transactions')
 cursor.execute('DELETE FROM sqlite_sequence WHERE name="transactions"')
 with open('transactions.csv', newline='', encoding='utf-8') as csvfile:
@@ -85,30 +117,26 @@ with open('transactions.csv', newline='', encoding='utf-8') as csvfile:
                        INSERT INTO transactions (AccountId, Date, Amount, Currency, DebitCredit, Status, Timezone)
                        VALUES (?, ?, ?, ?, ?, ?, ?)
                        ''', row[1:])
-"""
-# Print accounts   
-print("Accounts:")   
-cursor.execute('SELECT * FROM accounts')   
-for row in cursor.fetchall():   
-    print(row)   
-"""
-"""
-# Print transactions   
-print("\nTransactions:")   
-cursor.execute('SELECT * FROM transactions')   
-for row in cursor.fetchall():   
-    print(row)   
-"""
-"""
-cursor.execute('SELECT AccountId, TransactionId FROM transactions')
-rows = cursor.fetchall()
-rows_todict = dict(rows)
-print(rows_todict)
-"""
+    ####
+####
+####
 
+
+#### Untouched Transactions table
+####
+####
+cursor.execute('CREATE TABLE  IF NOT EXISTS orig_transactions AS SELECT * FROM transactions')
+####
+####
+####
+
+
+#### Map TransactionIds to AccountIds
+####
+####
 cursor.execute('SELECT TransactionId,AccountId FROM transactions')
-rows = cursor.fetchall()
-map_ids = dict(rows)
+rows0 = cursor.fetchall()
+map_ids = dict(rows0)
 
 flipped_map_ids = {}
 
@@ -118,50 +146,519 @@ for key, value in map_ids.items():
     else:
         flipped_map_ids[value].append(key)
 # print(flipped_map_ids)
+####
+####
 
+#### Count number of transactions
+####
+####
 cursor.execute('SELECT COUNT(TransactionId) from transactions')
-res1 = cursor.fetchall()
-res1 = list(itertools.chain(*res1))[0]
-
-cursor.execute('SELECT COUNT(DISTINCT DATE) AS NumUnDates from transactions')
-res2 = cursor.fetchall()
-res2 = list(itertools.chain(*res2))[0]
-print(f"{res1} transactions, from which {res1 - res2} have duplicate dates.")
-
-cursor.execute('SELECT DISTINCT DATE AS NumUnDates from transactions')
-res_disdates = cursor.fetchall()
-list_uniquedates = []
-for row in res_disdates:
-    list_uniquedates.append(row[0])
-
-dict_dates = {}
-for unique_date in list_uniquedates:
-    cursor.execute('SELECT TransactionId, AccountId, Date FROM transactions WHERE Date = ?', (unique_date,))
-    rows = cursor.fetchall()
-    dict_dates[unique_date] = rows
+num_transactions = cursor.fetchall()
+num_transactions = list(itertools.chain(*num_transactions))[0]
+####
+####
+####
 
 
-# print(dict_dates)
-def keys_with_multiple_values(d):
-    result = []
-    for key, value in d.items():
-        if isinstance(value, list) and len(value) > 1:
-            result.append(key)
-    return result
+#### More than one transaction for the same account on the same date
+####
+####
+cursor.execute('''
+               SELECT Date, AccountId, COUNT (*) as cnt
+               FROM transactions
+               GROUP BY Date, AccountId
+               HAVING cnt > 1
+               ''')
+dup_dates = cursor.fetchall()
+dup_dates_list = [row[0] for row in dup_dates]
+####
+####
 
 
-keys = keys_with_multiple_values(dict_dates)
-num_dupdates = len(keys)
-print(f"{num_dupdates} dates are duplicated and {res1 - res2 - num_dupdates} other are empty")
+#### Duplicated rows with the highest id
+####
+####
+cursor.execute('''
+               SELECT t.*
+               FROM transactions t
+                        JOIN (SELECT Date, MIN (TransactionId) AS min_id
+                              FROM transactions
+                              GROUP BY Date
+                              HAVING COUNT (*) > 1) sub
+                             ON t.Date = sub.Date AND t.TransactionId = sub.min_id
+               ORDER BY t.Date ASC
+               ''')
+# print(txns_dupdates_list)
+####
+####
+####
 
-for key in keys:
-    transaction_idlist = dict_dates.get(key)
-    for transaction_id in transaction_idlist:
-        transaction_id = transaction_id[0]
-        cursor.execute('SELECT TransactionId, AccountId, Date FROM transactions WHERE TransactionId= ?',
-                       (transaction_id,))
-        rows = cursor.fetchall()
-        print(rows)
+
+#### Query to create table with duplicated transactions
+####
+####
+cursor.execute('''
+               SELECT t.*
+               FROM transactions t
+                        JOIN (SELECT Date, AccountId, MIN (TransactionId) AS min_id
+                              FROM transactions
+                              GROUP BY Date, AccountId
+                              HAVING COUNT (*) > 1) sub
+                             ON t.Date = sub.Date AND t.AccountId = sub.AccountId
+               WHERE t.TransactionId != sub.min_id
+               ORDER BY t.Date ASC
+               ''')
+txns_dupdates = cursor.fetchall()
+txns_dupdates_list = [row[0:2] for row in txns_dupdates]
+
+#### Table with duplicated transactions
+####
+####
+cursor.execute('''CREATE TABLE IF NOT EXISTS DuplicateDates
+(
+    TransactionId
+    INT
+    PRIMARY
+    KEY,
+    AccountId
+    INT,
+    Date
+    DATETIME,
+    Amount
+    INT,
+    Currency
+    INT,
+    DebitCredit
+    VARCHAR
+                  (
+    10
+                  ),
+    Status VARCHAR
+                  (
+                      255
+                  ),
+    Timezone VARCHAR
+                  (
+                      255
+                  )
+    )''')
+####
+####
+####
+
+
+#### Fill Table with duplicated transactions
+####
+####
+cursor.execute('DELETE FROM DuplicateDates')
+insert_query = '''
+               INSERT INTO DuplicateDates
+               (TransactionId, AccountId, Date, Amount, Currency, DebitCredit, Status, Timezone)
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?) \
+               '''
+cursor.executemany(insert_query, txns_dupdates)
+####
+####
+####
+
+
+#### Remove duplicated transactions from the table transactions
+####
+####
+cursor.execute('''DELETE
+                  FROM transactions
+                  WHERE TransactionId IN (SELECT TransactionId
+                                          FROM DuplicateDates)''')
+####
+####
+####
+
+column_names_txn = [
+    'TransactionId',
+    'AccountId',
+    'Date',
+    'Amount',
+    'Currency',
+    'DebitCredit',
+    'Status',
+    'Timezone'
+]
+
+column_names_acc = [
+    'AccountId',
+    'Status',
+    'Currency',
+    'OpeningDate',
+    'AccountTypeCode',
+    'Timezone'
+]
+
+list_null_txn = []
+for col in column_names_txn:
+    query = f'SELECT COUNT(*) FROM transactions WHERE {col} IS NULL'
+    cursor.execute(query)
+    null_count = cursor.fetchone()[0]
+    list_null_txn.append(null_count)
+
+list_empty_txn = []
+for col in column_names_txn:
+    query0 = f'SELECT COUNT(*) AS CountEmpty FROM transactions WHERE {col} = ""'
+    cursor.execute(query0)
+    empty_count = cursor.fetchone()[0]
+    list_empty_txn.append(empty_count)
+
+list_empty_null_txn = list_empty_txn + list_empty_txn
+
+#### Handle empty AccountIds in the transactions table
+####
+####
+list_empty_accid = []
+if list_empty_null_txn[1] != 0:
+    query1 = f'SELECT TransactionId FROM transactions WHERE AccountId = ""'
+    cursor.execute(query1)
+    list_empty_accid = [row[0] for row in cursor.fetchall()]
+    print(list_empty_accid)
+# for aid in list_empty_accid:
+#   query = f'SELECT TransactionId FROM transactions WHERE AccountId = ""
+####
+####
+####
+
+list_null_acc = []
+for col in column_names_acc:
+    query = f'SELECT COUNT(*) FROM accounts WHERE {col} IS NULL'
+    cursor.execute(query)
+    null_count = cursor.fetchone()[0]
+    list_null_acc.append(null_count)
+
+list_empty_acc = []
+for col in column_names_acc:
+    query = f'SELECT COUNT(*) FROM accounts WHERE {col} = ""'
+    cursor.execute(query)
+    empty_count = cursor.fetchone()[0]
+    list_empty_acc.append(empty_count)
+print(list_empty_acc)
+
+# any empty string encountered is not a valid value, but rather a missing value
+for col in column_names_txn:
+    query = f'UPDATE transactions SET {col} = NULL WHERE {col} = ""'
+    cursor.execute(query)
+
+for col in column_names_acc:
+    query = f'UPDATE accounts SET {col} = NULL WHERE {col} = ""'
+    cursor.execute(query)
+
+conn.commit()
+conn.close()import sqlite3
+import csv
+import itertools
+
+conn = sqlite3.connect('test_db.db')
+conn.execute("PRAGMA foreign_keys = ON")
+cursor = conn.cursor()
+
+
+#### Accounts table
+####
+####
+cursor.execute('''   
+CREATE TABLE IF NOT EXISTS accounts (   
+    AccountId INTEGER PRIMARY KEY AUTOINCREMENT,   
+    Status VARCHAR(255),   
+    Currency INTEGER,   
+    OpeningDate DATETIME,   
+    AccountTypeCode CHAR(10),   
+    Timezone VARCHAR(255)   
+)   
+''')
+####
+####
+####
+
+
+
+#### Fill accounts table
+####
+####
+cursor.execute('DELETE FROM accounts')
+cursor.execute('DELETE FROM sqlite_sequence WHERE name="accounts"')
+with open('accounts.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    for row in reader:
+        cursor.execute('''   
+            INSERT INTO accounts (Status, Currency, OpeningDate, AccountTypeCode, Timezone)  
+            VALUES (?, ?, ?, ?, ?)   
+        ''', row[1:])
+####
+####
+####
+
+
+#### Untouched Accounts table
+####
+####
+cursor.execute('CREATE TABLE IF NOT EXISTS orig_accounts AS SELECT * FROM accounts')
+####
+####
+####
+
+
+####  Transactions table
+####
+####
+cursor.execute('''   
+CREATE TABLE IF NOT EXISTS transactions (   
+    TransactionId INTEGER PRIMARY KEY AUTOINCREMENT,  
+    AccountId INTEGER, 
+    Date DATETIME,     
+    Amount REAL,    
+    Currency INTEGER,   
+    DebitCredit TEXT,   
+    Status TEXT,   
+    Timezone TEXT, 
+    FOREIGN KEY (AccountId) REFERENCES accounts(AccountId) 
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+)   
+''')
+
+
+
+#### Fill transactions table
+####
+####
+cursor.execute('DELETE FROM transactions')
+cursor.execute('DELETE FROM sqlite_sequence WHERE name="transactions"')
+with open('transactions.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    for row in reader:
+        cursor.execute('''   
+            INSERT INTO transactions (AccountId, Date, Amount, Currency, DebitCredit, Status, Timezone)  
+            VALUES (?, ?, ?, ?, ?, ?, ?)  
+        ''', row[1:])
+####
+####
+####
+
+
+
+#### Untouched Transactions table
+####
+####
+cursor.execute('CREATE TABLE  IF NOT EXISTS orig_transactions AS SELECT * FROM transactions')
+####
+####
+####
+
+
+
+#### Map TransactionIds to AccountIds
+####
+####
+cursor.execute('SELECT TransactionId,AccountId FROM transactions')
+rows0 = cursor.fetchall()
+map_ids = dict(rows0)
+
+flipped_map_ids= {}
+
+for key, value in map_ids.items():
+    if value not in flipped_map_ids:
+        flipped_map_ids[value] = [key]
+    else:
+        flipped_map_ids[value].append(key)
+#print(flipped_map_ids)
+####
+####
+
+#### Count number of transactions
+####
+####
+cursor.execute('SELECT COUNT(TransactionId) from transactions')
+num_transactions = cursor.fetchall()
+num_transactions = list(itertools.chain(*num_transactions))[0]
+####
+####
+####
+
+
+#### More than one transaction for the same account on the same date
+####
+####
+cursor.execute(''' 
+    SELECT Date, AccountId, COUNT(*) as cnt   
+    FROM transactions   
+    GROUP BY Date, AccountId   
+    HAVING cnt > 1 
+''')
+dup_dates = cursor.fetchall()
+dup_dates_list = [row[0] for row in dup_dates]
+####
+####
+
+
+#### Duplicated rows with the highest id
+####
+####
+cursor.execute(''' 
+    SELECT t.* 
+    FROM transactions t 
+    JOIN ( 
+        SELECT Date, MIN(TransactionId) AS min_id 
+        FROM transactions 
+        GROUP BY Date 
+        HAVING COUNT(*) > 1 
+    ) sub 
+    ON t.Date = sub.Date AND t.TransactionId = sub.min_id 
+    ORDER BY t.Date ASC 
+''')
+#print(txns_dupdates_list)
+####
+####
+####
+
+
+
+#### Query to create table with duplicated transactions
+####
+####
+cursor.execute('''
+    SELECT t.* 
+    FROM transactions t 
+    JOIN ( 
+        SELECT Date, AccountId, MIN(TransactionId) AS min_id 
+        FROM transactions 
+        GROUP BY Date, AccountId 
+        HAVING COUNT(*) > 1 
+    ) sub 
+    ON t.Date = sub.Date AND t.AccountId = sub.AccountId 
+    WHERE t.TransactionId != sub.min_id 
+    ORDER BY t.Date ASC
+''')
+txns_dupdates= cursor.fetchall()
+txns_dupdates_list = [row[0:2] for row in txns_dupdates]
+
+#### Table with duplicated transactions
+####
+####
+cursor.execute('''CREATE TABLE IF NOT EXISTS DuplicateDates ( 
+    TransactionId INT PRIMARY KEY, 
+    AccountId INT, 
+    Date DATETIME, 
+    Amount INT, 
+    Currency INT, 
+    DebitCredit VARCHAR(10), 
+    Status VARCHAR(255), 
+    Timezone VARCHAR(255) 
+)''')
+####
+####
+####
+
+
+
+#### Fill Table with duplicated transactions
+####
+####
+cursor.execute('DELETE FROM DuplicateDates')
+insert_query = ''' 
+    INSERT INTO DuplicateDates 
+    (TransactionId, AccountId, Date, Amount, Currency, DebitCredit, Status, Timezone) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?) 
+'''
+cursor.executemany(insert_query, txns_dupdates)
+####
+####
+####
+
+
+
+#### Remove duplicated transactions from the table transactions
+####
+####
+cursor.execute('''DELETE FROM transactions 
+WHERE TransactionId IN ( 
+    SELECT TransactionId FROM DuplicateDates 
+)''')
+####
+####
+####
+
+column_names_txn = [
+    'TransactionId',
+    'AccountId',
+    'Date',
+    'Amount',
+    'Currency',
+    'DebitCredit',
+    'Status',
+    'Timezone'
+]
+
+column_names_acc = [
+    'AccountId',
+    'Status',
+    'Currency',
+    'OpeningDate',
+    'AccountTypeCode',
+    'Timezone'
+]
+
+list_null_txn = []
+for col in column_names_txn:
+    query = f'SELECT COUNT(*) FROM transactions WHERE {col} IS NULL'
+    cursor.execute(query)
+    null_count = cursor.fetchone()[0]
+    list_null_txn.append(null_count)
+
+list_empty_txn = []
+for col in column_names_txn:
+    query0 = f'SELECT COUNT(*) AS CountEmpty FROM transactions WHERE {col} = ""'
+    cursor.execute(query0)
+    empty_count = cursor.fetchone()[0]
+    list_empty_txn.append(empty_count)
+
+list_empty_null_txn = list_empty_txn+list_empty_txn
+
+#### Handle empty AccountIds in the transactions table
+####
+####
+list_empty_accid = []
+if list_empty_null_txn[1] != 0:
+    query1 = f'SELECT TransactionId FROM transactions WHERE AccountId = ""'
+    cursor.execute(query1)
+    list_empty_accid = [row[0] for row in cursor.fetchall()]
+    print(list_empty_accid)
+#for aid in list_empty_accid:
+#   query = f'SELECT TransactionId FROM transactions WHERE AccountId = ""
+####
+####
+####
+
+list_null_acc = []
+for col in column_names_acc:
+    query = f'SELECT COUNT(*) FROM accounts WHERE {col} IS NULL'
+    cursor.execute(query)
+    null_count = cursor.fetchone()[0]
+    list_null_acc.append(null_count)
+
+list_empty_acc = []
+for col in column_names_acc:
+    query = f'SELECT COUNT(*) FROM accounts WHERE {col} = ""'
+    cursor.execute(query)
+    empty_count = cursor.fetchone()[0]
+    list_empty_acc.append(empty_count)
+print(list_empty_acc)
+
+# any empty string encountered is not a valid value, but rather a missing value
+for col in column_names_txn:
+    query = f'UPDATE transactions SET {col} = NULL WHERE {col} = ""'
+    cursor.execute(query)
+
+for col in column_names_acc:
+    query = f'UPDATE accounts SET {col} = NULL WHERE {col} = ""'
+    cursor.execute(query)
 
 conn.commit()
 conn.close()
