18fc413f1939e44881254c7080b89806325f6d89
Lorena Ortiz <a01750283@tec.mx>
2026-05-21 21:07:48 +0200

    Fix issues

--- testproject/sql-duplicates.py
+++ testproject/sql-duplicates.py
@@ -6,12 +6,18 @@ conn = sqlite3.connect('test_db.db')
 conn.execute("PRAGMA foreign_keys = ON")
 cursor = conn.cursor()
 
-# Count number of transactions
-cursor.execute('SELECT COUNT(TransactionId) from transactions')
+#### Count number of transactions
+
+cursor.execute('SELECT COUNT(TransactionId) FROM transactions')
 num_transactions = cursor.fetchall()
 num_transactions = list(itertools.chain(*num_transactions))[0]
 
-# More than one transaction for the same account on the same date
+####
+####
+####
+
+#### More than one transaction for the same account on the same date
+
 cursor.execute(''' 
     SELECT Date, AccountId, COUNT(*) as cnt   
     FROM transactions   
@@ -21,7 +27,11 @@ cursor.execute('''
 dup_dates = cursor.fetchall()
 dup_dates_list = [row[0] for row in dup_dates]
 
-# Duplicated rows with the lowest id
+####
+####
+
+#### Duplicated rows with the lowest id
+
 cursor.execute(''' 
     SELECT t.* 
     FROM transactions t 
@@ -34,8 +44,15 @@ cursor.execute('''
     ON t.Date = sub.Date AND t.TransactionId = sub.min_id 
     ORDER BY t.Date ASC 
 ''')
+# print(txns_dupdates_list)
 
-# Query to create table with duplicated transactions
+####
+####
+####
+
+#### Query to create table with duplicated transactions
+#### transactions on the same date by the same account
+
 cursor.execute('''
     SELECT t.* 
     FROM transactions t 
@@ -52,32 +69,45 @@ cursor.execute('''
 txns_dupdates = cursor.fetchall()
 txns_dupdates_list = [row[0:2] for row in txns_dupdates]
 
-# Table with duplicated transactions
+#### Table with duplicated transactions
+
 cursor.execute('''CREATE TABLE IF NOT EXISTS DuplicateDates ( 
     TransactionId INT PRIMARY KEY, 
     AccountId INT, 
     Date DATETIME, 
     Amount INT, 
-    Currency INT, 
     DebitCredit VARCHAR(10), 
     Status VARCHAR(255), 
     Timezone VARCHAR(255) 
 )''')
 
-# Fill Table with duplicated transactions
+####
+####
+####
+
+#### Fill Table with duplicated transactions
+
 cursor.execute('DELETE FROM DuplicateDates')
 insert_query = ''' 
     INSERT INTO DuplicateDates 
-    (TransactionId, AccountId, Date, Amount, Currency, DebitCredit, Status, Timezone) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?) 
+    (TransactionId, AccountId, Date, Amount, DebitCredit, Status, Timezone) 
+    VALUES (?, ?, ?, ?, ?, ?, ?) 
 '''
 cursor.executemany(insert_query, txns_dupdates)
 
-# Remove duplicated transactions from the table transactions
+####
+####
+####
+
+#### Remove duplicated transactions from the table transactions
+
 cursor.execute('''DELETE FROM transactions 
 WHERE TransactionId IN ( 
     SELECT TransactionId FROM DuplicateDates 
 )''')
 
+####
+####
+####
 conn.commit()
 conn.close()
--- testproject/sql-filltables.py
+++ testproject/sql-filltables.py
@@ -1,24 +1,27 @@
 import sqlite3
 import csv
 import itertools
+from datetime import datetime
+from dateutil import parser
 
 conn = sqlite3.connect('test_db.db')
 conn.execute("PRAGMA foreign_keys = ON")
 cursor = conn.cursor()
 
-# Accounts table
+#### Accounts table
 cursor.execute('''   
 CREATE TABLE IF NOT EXISTS accounts (   
     AccountId INTEGER PRIMARY KEY AUTOINCREMENT,   
     Status VARCHAR(255),   
-    Currency VARCHAR(3),   
+    Currency INTEGER,   
     OpeningDate DATETIME,   
     AccountTypeCode CHAR(10),   
     Timezone VARCHAR(255)   
 )   
 ''')
 
-# Fill accounts table
+#### Fill accounts table
+# cursor.execute('DELETE FROM transactions')
 cursor.execute('DELETE FROM accounts')
 cursor.execute('DELETE FROM sqlite_sequence WHERE name="accounts"')
 
@@ -31,18 +34,16 @@ with open('accounts.csv', newline='', encoding='utf-8') as csvfile:
             VALUES (?, ?, ?, ?, ?)   
         ''', row[1:])
 
-# Untouched Accounts table
-cursor.execute('DROP TABLE IF EXISTS orig_accounts')
-cursor.execute('CREATE TABLE orig_accounts AS SELECT * FROM accounts')
+#### Untouched Accounts table
+cursor.execute('CREATE TABLE IF NOT EXISTS orig_accounts AS SELECT * FROM accounts')
 
-# Transactions table
+#### Transactions table
 cursor.execute('''   
 CREATE TABLE IF NOT EXISTS transactions (   
     TransactionId INTEGER PRIMARY KEY AUTOINCREMENT,  
     AccountId INTEGER NOT NULL, 
     Date DATETIME,     
     Amount INTEGER,     
-    Currency VARCHAR(3),   
     DebitCredit TEXT,   
     Status TEXT,   
     Timezone TEXT, 
@@ -52,7 +53,7 @@ CREATE TABLE IF NOT EXISTS transactions (
 )   
 ''')
 
-# Fill transactions table
+#### Fill transactions table
 cursor.execute('DELETE FROM transactions')
 cursor.execute('DELETE FROM sqlite_sequence WHERE name="transactions"')
 
@@ -63,8 +64,8 @@ with open('transactions.csv', newline='', encoding='utf-8') as csvfile:
     for row in reader:
         try:
             cursor.execute(''' 
-                INSERT INTO transactions (AccountId, Date, Amount, Currency, DebitCredit, Status, Timezone) 
-                VALUES (?, ?, ?, ?, ?, ?, ?) 
+                INSERT INTO transactions (AccountId, Date, Amount, DebitCredit, Status, Timezone) 
+                VALUES (?, ?, ?, ?, ?, ?) 
             ''', row[1:])
         except sqlite3.IntegrityError as e:
             if len(row) > 1:
@@ -74,12 +75,11 @@ with open('transactions.csv', newline='', encoding='utf-8') as csvfile:
             else:
                 list_empty_accid.append(row)
 
-# Untouched Transactions table
-cursor.execute('DROP TABLE IF EXISTS orig_transactions')
-cursor.execute('CREATE TABLE orig_transactions AS SELECT * FROM transactions')
+#### Untouched Transactions table
+cursor.execute('CREATE TABLE IF NOT EXISTS orig_transactions AS SELECT * FROM transactions')
 
-# Map TransactionIds to AccountIds
-cursor.execute('SELECT TransactionId,AccountId FROM transactions')
+#### Map TransactionIds to AccountIds
+cursor.execute('SELECT TransactionId, AccountId FROM transactions')
 rows0 = cursor.fetchall()
 map_ids = dict(rows0)
 
@@ -90,6 +90,7 @@ for key, value in map_ids.items():
         flipped_map_ids[value] = [key]
     else:
         flipped_map_ids[value].append(key)
+# print(flipped_map_ids)
 
 conn.commit()
 conn.close()
